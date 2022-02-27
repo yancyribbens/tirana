3292653f4a5c468d3f94034c1b34e712c9ab170e
yancy <email@yancy.lol>
2022-02-27 14:23:55 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,18 @@ use tokio_stream::StreamExt;
 use tokio_util::codec::{BytesCodec, Decoder};
 use futures::join;
 
-use irc_proto::command::*;
-use irc_proto::message::Message;
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 
-async fn handle_inbound(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+use irc_proto::{
+    message::Message,
+    command::*,
+};
+
+type conduit = Arc<Mutex<HashMap<String, Bytes>>>;
+
+async fn handle_inbound(mut write_half: WriteHalf<'_>) -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("127.0.0.1:31415").await?;
 
     loop {
@@ -24,7 +32,7 @@ async fn handle_inbound(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
 
         while let Some(message) = framed.next().await {
             match message {
-                Ok(bytes) => { stream.write(&bytes).await.unwrap(); println!("bytes: {:?}", bytes) },
+                Ok(bytes) => { write_half.write(&bytes).await.unwrap(); println!("bytes: {:?}", bytes) },
                 Err(err) => println!("Socket closed with error: {:?}", err),
             }
         }
@@ -33,30 +41,31 @@ async fn handle_inbound(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     }
 }
 
-async fn irc_stdout(stream: tokio::net::TcpStream) {
+fn process_buf(buf: &Vec<u8>) {
+	let msg_str = str::from_utf8(&buf).unwrap();
+	let irc_message = msg_str.parse::<Message>().unwrap();
+	match irc_message.command {
+		Command::PING(ref server, ref _server_two) => {
+			println!("PING: {}", server);
+			//let cmd = Command::new("PONG", vec![server]).unwrap();
+			//let irc_message = format!(
+				//"{}\n", IrcMessage::from(cmd).to_string());
+		},
+		_ => println!("{}", irc_message),
+	}
+
+	println!("{}", irc_message);
+}
+
+async fn irc_stdout(read_half: ReadHalf<'_>) {
     loop {
-        stream.readable().await.expect("oops");
+        read_half.readable().await.expect("oops");
 
         let mut buf = Vec::with_capacity(4096);
 
-        match stream.try_read_buf(&mut buf) {
+        match read_half.try_read_buf(&mut buf) {
             Ok(0) => break,
-            Ok(n) => {
-				//let message = line.parse::<IrcMessage>().unwrap();
-
-				let msg = str::from_utf8(&buf).unwrap();
-				let message = msg.parse::<Message>().unwrap();
-				println!("read {}", message);
-			
-				match message.command {
-					Command::PING(ref server, ref _server_two) => {
-						println!("got ping");
-						let cmd = Command::new("PONG", vec![server]).unwrap();
-					},
-
-					_ => continue,
-				}
-            }
+            Ok(n) => process_buf(&buf),
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                 continue;
             }
@@ -71,13 +80,13 @@ async fn irc_stdout(stream: tokio::net::TcpStream) {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mut stream: TcpStream = TcpStream::connect("irc.libera.chat:6665").await.unwrap();
-    //let (mut read_half, mut write_half) = stream.split();
-    stream.write(b"NICK test31415\n").await.unwrap();
-    stream.write(b"USER test31415 0 * :Ronnie Reagan\n").await.unwrap();
-	let h1 = handle_inbound(stream);
-    let h2 = irc_stdout(stream);
+    let (mut read_half, mut write_half) = stream.split();
+    write_half.write(b"NICK test31415\n").await.unwrap();
+    write_half.write(b"USER test31415 0 * :Ronnie Reagan\n").await.unwrap();
+    let h1 = handle_inbound(write_half);
+    let h2 = irc_stdout(read_half);
 
-    //join!(h1, h2);
+    join!(h1, h2);
 
     Ok(())
 }
